3033b5d1482655bf6e1d5d9baac2f0ddf0738859
KanameAraki <kana911me@gmail.com>
2021-05-11 02:35:22 +0000

    [Add]検索機能の追加

--- app/controllers/public/games_controller.rb
+++ app/controllers/public/games_controller.rb
@@ -1,12 +1,21 @@
 class Public::GamesController < ApplicationController
 
   def index
-    @games = Game.all
+    if params[:search]
+      @games = Game.search(params[:search])
+      # binding.pry
+    else
+      @games = Game.all
+    end
   end
 
   def show
     @game = Game.find(params[:id])
+    if params[:search] == "1"
+    @posts = Post.where(game_id: @game.id, category: "recruitment")
+    else
     @posts = Post.where(game_id: @game.id)
+    end
+    # binding.pry
   end
-
 end
